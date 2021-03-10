8d531e8cac2b7e344db2221f20dfe2c8d028152c
ViniciusHungaro <hungaro545@gmail.com>
2021-03-09 21:58:54 -0300

    Titulo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,13 +9,7 @@ import styles from '../styles/components/Home.module.css';
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Vinícius</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
       <SideBar />
-      
     </div>
   )
 }
