06f7a33dc95cffa580673369b2435215605df76e
ViniciusHungaro <hungaro545@gmail.com>
2021-03-09 21:49:40 -0300

    Foto e titulo

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -7,7 +7,7 @@ import { faFacebookSquare, faGithubSquare, faInstagramSquare }  from '@fortaweso
 export function SideBar() {
     return (
         <div className={styles.container}>
-            <img src="https://scontent.fmii4-1.fna.fbcdn.net/v/t31.0-8/17973867_1208143242646825_5304037149618716966_o.jpg?_nc_cat=106&ccb=3&_nc_sid=09cbfe&_nc_ohc=_VGBnOU1YOUAX99uCBr&_nc_ht=scontent.fmii4-1.fna&oh=d4cc75856b0c7c7968b599078c603dad&oe=60682DA3" alt=""/>
+            <img src="https://cdn0.iconfinder.com/data/icons/set-ui-app-android/32/8-512.png" alt="Vinicius Hungaro"/>
             <p>Vinícius Moreira Hungaro</p>
 
             <div className={styles.menu}>
